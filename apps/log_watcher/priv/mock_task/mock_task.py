5752173c4d35b55714625a6d09dbe2b88085a230
Peter Zingg <peter.zingg@gmail.com>
2021-08-02 17:37:11 -0700

    traceback and error logging for python

--- apps/log_watcher/priv/mock_task/mock_task.py
+++ apps/log_watcher/priv/mock_task/mock_task.py
@@ -5,6 +5,10 @@ import os
 import signal
 import sys
 import time
+import traceback
+
+class CancelException(RuntimeError):
+  pass
 
 # TODO: put these in -log.jsonl, -start.json, and/or -result.json
 # 'time' 
@@ -36,7 +40,7 @@ def read_arg_file(info, arg_file):
 
     return (info, args)
 
-def mock_status(info, line_no, num_lines, error, cancel):
+def mock_status(info, line_no, num_lines, error):
   raise_error = False
   progress_counter = None
   progress_total = None
@@ -57,10 +61,6 @@ def mock_status(info, line_no, num_lines, error, cancel):
     if line_no == 4:
       if error == 'running':
         raise_error = True
-      else:
-        if cancel:
-          status = 'canceled'
-          errors = [{'message': f'canceled on line {line_no}'}]
   else:
     status = 'completed'
     result = {'params': [['a', 2], ['b', line_no]]}
@@ -69,6 +69,7 @@ def mock_status(info, line_no, num_lines, error, cancel):
   info['time'] = format_utcnow()
   info['status'] = status
   info['message'] = f'Task {task_id} {status} on line {line_no}'
+  set_script_status(status)
 
   if progress_counter is not None and progress_total is not None:
     info['progress_counter'] = progress_counter
@@ -101,23 +102,29 @@ ARG_KEYS = ['time', 'session_id', 'task_id', 'task_type', 'gen']
 
 def write_start_file(start_file, info):
   path = os.path.join(info['session_log_path'], start_file)
-  with open(path, 'wt') as f:
-    json.dump(info, f, indent=2)
-    f.write('\n')
+  try:
+    with open(path, 'wt') as f:
+      json.dump(info, f, indent=2)
+      f.write('\n')
+  except:
+    pass
 
   log_event('task_started', info)
 
 def write_result_file(result_file, info, result_data):
   path = os.path.join(info['session_log_path'], result_file)
-  with open(path, 'wt') as f:
-    result_info = {k: v for k, v in info.items() if k not in ['result']}
-    result_info['result'] = {
-      'succeeded': info['result']['succeeded'],
-      'errors': info['result']['errors'],
-      'data': result_data
-    }
-    json.dump(result_info, f, indent=2)
-    f.write('\n')
+  try:
+    with open(path, 'wt') as f:
+      result_info = {k: v for k, v in info.items() if k not in ['result']}
+      result_info['result'] = {
+        'succeeded': info['result']['succeeded'],
+        'errors': info['result']['errors'],
+        'data': result_data
+      }
+      json.dump(result_info, f, indent=2)
+      f.write('\n')
+  except:
+    pass
 
   event_type = f'''task_{info['status']}'''
   log_event(event_type, info)
@@ -126,108 +133,192 @@ def log_event(event_type, info):
   info['event_type'] = event_type
   log_file = f'''{info['session_id']}-sesslog.jsonl'''
   path = os.path.join(info['session_log_path'], log_file)
-  with open(path, 'at') as f:
+  try:
+    f = open(path, 'at')
     json.dump(info, f)
     f.write('\n')
+    f.close()
+  except:
+    pass
+
+def make_error_list(errors, system = True, fatal = True):
+  global _global_args
+  category = _global_args['status']
+  return [{
+    'message': error,
+    'category': category,
+    'system': system,
+    'fatal': fatal} for error in errors]
+
+def get_traceback(error_type):
+  _type, value, tb = sys.exc_info()
+  tb_summary = traceback.extract_tb(tb)
+  tb_list = [frame.line for frame in tb_summary]
+
+  # tb_stack = traceback.extract_stack(limit=7)
+  # tb_list = traceback.format_list(tb_stack)
+  # tb_list = [text for (file, line, func, text) in tb_stack]
+
+  if error_type == 'interrupt':
+    stack_msg = tb_list[0:-2]
+  else:
+    stack_msg = tb_list
+  info = {
+    'error_type': error_type,
+    'error': str(value),
+    'call': tb_summary[-1].name,
+    'traceback': stack_msg
+  }
+  return info
+
+def log_error(error_type):
+  global _global_args
+
+  trace = get_traceback(error_type)
+  message = trace['error']
+
+  result_file = result_file_name(_global_args['task_id'], _global_args['task_type'], _global_args['gen'])
+
+  result_info = {
+    'succeeded': False,
+    'file': result_file,
+    'errors': make_error_list([message])
+  }
+
+  if trace['error_type'] == 'interrupt':
+    status = 'cancelled'
+  else:
+    status = 'completed'
+
+  set_script_status(status)
+  info = {
+    'time': format_utcnow(), 
+    'os_pid': _global_args['os_pid'],
+    'session_id': _global_args['session_id'],
+    'session_log_path': _global_args['log_path'],
+    'task_id': _global_args['task_id'],
+    'task_type': _global_args['task_type'],
+    'gen': _global_args['gen'],
+    'message': message,
+    'status': status,
+    'result': result_info,
+    'call': trace['call'],
+    'traceback': trace['traceback']
+  }
+  write_result_file(result_file, info, None)
+  if _global_args['log_f']:
+    log_info(info, _global_args['log_f'], suppress_exception = True)
+
+def set_script_status(status):
+  global _global_args
+  _global_args['status'] = status
 
 def cancel_task(signal_number, frame):
-  # TODO: set status to "canceled", and write_result_file()
-  sys.exit(2)
+  raise CancelException(f'signal #{signal_number} received')
 
-def run_job(args):
-  session_log_path = args['log_path']
-  session_id = args['session_id']
-  task_id = args['task_id']
-  task_type = args['task_type']
-  gen = args['gen']
-  error = args['error']
-  cancel = args['cancel']
+def run_job():
+  global _global_args
+  session_log_path = _global_args['log_path']
+  session_id = _global_args['session_id']
+  task_id = _global_args['task_id']
+  task_type = _global_args['task_type']
+  gen = _global_args['gen']
+  error = _global_args['error']
+  os_pid = _global_args['os_pid']
 
   log_file = log_file_name(task_id, task_type, gen)
-  with open(os.path.join(session_log_path, log_file), 'wt') as f:
-    os_pid = os.getpid()
-    started_at = None
-    running_at = None
-    write_start = False
-    write_result = False
+  log_file_path = os.path.join(session_log_path, log_file)
+  _global_args['log_f'] = log_file_path
+  started_at = None
+  running_at = None
+  write_start = False
+  write_result = False
 
-    info = {
-      'time': format_utcnow(), 
-      'os_pid': os_pid,
-      'session_id': session_id, 
-      'session_log_path': session_log_path,
-      'task_id': task_id, 
-      'task_type': task_type, 
-      'gen': gen,
-      'status': 'created',
-      'message': f'Task {task_id} created'
-    }
-    print(f'mock_task, writing initial log file')
-    flush_info(info, f)
-    log_event('task_created', info)
+  set_script_status('created')
+  info = {
+    'time': format_utcnow(), 
+    'os_pid': os_pid,
+    'session_id': session_id, 
+    'session_log_path': session_log_path,
+    'task_id': task_id, 
+    'task_type': task_type, 
+    'gen': gen,
+    'status': 'created',
+    'message': f'Task {task_id} created'
+  }
+  print(f'mock_task, writing initial log file')
+  log_info(info, log_file_path, initial = True)
+  log_event('task_created', info)
 
-    signal.signal(signal.SIGUSR1, cancel_task)
+  signal.signal(signal.SIGINT, cancel_task)
 
-    arg_file = arg_file_name(task_id, task_type, gen)
-    info, task_args = read_arg_file(info, arg_file)
+  arg_file = arg_file_name(task_id, task_type, gen)
+  info, task_args = read_arg_file(info, arg_file)
 
-    print(f'mock_task, task_args are {task_args}')
-    flush_info(info, f)
+  print(f'mock_task, task_args are {task_args}')
+  log_info(info, log_file_path)
 
-    num_lines = task_args['num_lines']
-    for line_no in range(1, num_lines+1):
-      time.sleep(0.25)
-      info, result, errors = mock_status(info, line_no, num_lines, error, cancel)
+  num_lines = task_args['num_lines']
+  for line_no in range(1, num_lines+1):
+    info, result, errors = mock_status(info, line_no, num_lines, error)
 
-      if started_at is None:
-        info['started_at'] = started_at = info['time']
+    if started_at is None:
+      info['started_at'] = started_at = info['time']
 
-      if running_at is None and info['status'] in ['running', 'completed']:
-        info['running_at'] = running_at = info['time']
-        write_start = True
-    
-      if info['status'] in ['canceled', 'completed']:
-        result_file = result_file_name(task_id, task_type, gen)
-        if info['status'] == 'completed' and len(errors) == 0:
-          result_info = {
-            'succeeded': True,
-            'file': result_file,
-            'errors': errors
-          }
-        else:
-          result_info = {
-            'succeeded': False,
-            'file': result_file,
-            'errors': errors
-          }
-        info.update({
-          'completed_at': info['time'],
-          'result': result_info
-        })
-        write_result = True
+    if running_at is None and info['status'] in ['running', 'completed']:
+      info['running_at'] = running_at = info['time']
+      write_start = True
+  
+    if info['status'] in ['cancelled', 'completed']:
+      result_file = result_file_name(task_id, task_type, gen)
+      if info['status'] == 'completed' and len(errors) == 0:
+        result_info = {
+          'succeeded': True,
+          'file': result_file,
+          'errors': errors
+        }
+      else:
+        result_info = {
+          'succeeded': False,
+          'file': result_file,
+          'errors': errors
+        }
+      info.update({
+        'completed_at': info['time'],
+        'result': result_info
+      })
+      write_result = True
 
-      if write_start:
-        print(f'mock_task, writing start file')
-        start_file = start_file_name(task_id, task_type, gen)
-        write_start_file(start_file, info)
-        write_start = False
+    if write_start:
+      print(f'mock_task, writing start file')
+      start_file = start_file_name(task_id, task_type, gen)
+      write_start_file(start_file, info)
+      write_start = False
 
-      if write_result and result_file is not None:
-        print(f'mock_task, writing result file')
-        write_result_file(result_file, info, result)
-        write_result = False
+    if write_result and result_file is not None:
+      print(f'mock_task, writing result file')
+      write_result_file(result_file, info, result)
+      write_result = False
 
-      flush_info(info, f)
-      if write_result:
-        break
+    log_info(info, log_file_path)
+    if write_result:
+      break
+    time.sleep(2)
 
-    print(f'mock_task, closing log file')
+  print(f'mock_task, closing log file')
 
-
-def flush_info(info, f):
-  json.dump(info, f)
-  f.write('\n')
-  f.flush()
+def log_info(info, log_file_path, initial = False, suppress_exception = False):
+  mode = 'wt' if initial else 'at'
+  try:
+    f = open(log_file_path, mode)
+    json.dump(info, f)
+    f.write('\n')
+    f.close()
+  except OSError:
+    if initial and not suppress_exception:
+      raise RuntimeError(f'cannot open log file {log_file_path}')
+    else:
+      print(json.dumps(info))
 
 if __name__ == '__main__':
   import argparse
@@ -238,7 +329,16 @@ if __name__ == '__main__':
   parser.add_argument('-i', '--task-id', help='task id', required=True)
   parser.add_argument('-t', '--task-type', help='task type', required=True)
   parser.add_argument('-g', '--gen', help='gen', type=int, required=True)
-  parser.add_argument('-c', '--cancel', help='generate canceled result', action='store_true')
   parser.add_argument('-e', '--error', help='phase in which to generate error result')
   args = parser.parse_args()
-  run_job(vars(args))
+  _global_args = vars(args)
+  _global_args['os_pid'] = os_pid = os.getpid()
+  _global_args['status'] = 'initializing'
+  _global_args['log_f'] = None
+  print(f'script started. cancel with:\n  kill -s INT {os_pid}')
+  try:
+    run_job()
+  except CancelException:
+    log_error('interrupt')
+  except Exception:
+    log_error('error')
